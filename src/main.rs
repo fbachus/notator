2d3398da1b258d5de0afc815be578407152f0ebe
fbachus <felix.bachstein@gmail.com>
2024-02-08 20:10:05 +0100

    add styling, navbar, css file serving, and first page links

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,32 @@
 use rocket::http::{Status, ContentType};
 use rocket::response::{content, status};
 use rocket_dyn_templates::{Template, context};
+use rocket::fs::NamedFile;
 use yew::{Html, html};
+use std::path::PathBuf;
 
 #[derive(Responder)]
 #[response( status = 418, content_type = "json")]
 struct RawTeapotJson(&'static str);
+#[derive(Responder)]
+#[response( status = 404, content_type = "json")]
+struct NotFound(&'static str);
 
 //#[derive(Responder)]
 //#[response( status = 200, content_type = "HTML")]
 //struct WebpageContent(HTML);
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let title = String::from("Home");
+    Template::render("test_template_base",
+                     context! { title: title } )
+}
+
+#[get("/<css_file>", rank = 2)]
+    async fn serve_css(css_file: PathBuf) -> content::RawCss<NamedFile> {
+        let path: PathBuf = [PathBuf::from("./style"), css_file].iter().collect();
+        content::RawCss(NamedFile::open(path).await.expect("no file found"))
 }
 
 #[get("/response")]
@@ -26,13 +39,21 @@ fn teapot() -> RawTeapotJson {
 
 #[get("/template")]
 fn example() -> Template {
-    let users = vec!["mark", "ruffalo"];
-    Template::render("test_template_base", context! { users: users } )
+    let users = vec!["john", "lennon"];
+    let title = String::from("other Template");
+    Template::render("first_extend_template",
+                     context! { users: users, title: title } )
 }
-#[get("/template_override")]
-    fn template_2() -> Template {
-    let users = vec!["mark", "ruffalo"];
-    Template::render("first_extend_template", context! { users: users } )
+//#[get("/template_override_test_empty")]
+//fn template() -> Template {
+//    let users = vec!["john", "lennon"];
+//    let title = String::from("Notator");
+//    Template::render("first_extend_template",
+//                     context! { users: users, title: title } )
+//}
+#[get("/<_..>", rank = 12)]
+fn not_found() -> NotFound {
+    NotFound("could not find path")
 }
 
 #[launch]
@@ -41,6 +62,7 @@ fn rocket() -> _ {
         .mount("/", routes![index])
         .mount("/", routes![teapot])
         .mount("/", routes![example])
-        .mount("/", routes![template_2])
+        .mount("/style", routes![serve_css])
+        .mount("/", routes![not_found])
         .attach(Template::fairing())
 }
