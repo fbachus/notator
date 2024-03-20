b2b02b1ead2480cbab902ae447fb93848693d173
fbachus <felix.bachstein@gmail.com>
2024-03-21 00:21:29 +0100

    add some super super basic login without authentication

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 #[macro_use] extern crate rocket;
 
-use rocket::http::{Status, ContentType};
-use rocket::response::{content, status, Redirect};
-use rocket::request::{FromParam};
+use rocket::http::{Status, ContentType, CookieJar};
+use rocket::response::{content, status, Redirect, Response};
 use rocket::fs::{NamedFile, FileServer};
 use rocket::form::Form;
 use rocket::Request;
 use rocket_dyn_templates::{Template, context};
-use yew::{Html, html};
 use std::path::{Path,PathBuf};
-use chrono::Utc;
 
 #[derive(Responder)]
 #[response( status = 418, content_type = "json")]
@@ -22,27 +19,36 @@ struct NotFound(&'static str);
 struct BadRequest(&'static str);
 #[derive(Responder)]
 #[response( status = 201, content_type = "json")]
-struct Success(&'static str);
-#[derive(Responder)]
-#[response( status = 201, content_type = "json")]
 struct Created(&'static str);
 #[catch(400)]
 fn bad_request(req: &Request) -> String{
     format!("Invalid input")
+}
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Page or Resource could not be found: {}", req.uri())
 }
 
 #[derive(FromForm,Debug)]
 struct NewNote {
     content: String,
 }
-#[derive(FromForm)]
-struct CalcVariables {
+#[derive(FromForm, Debug)]
+struct User {
+    username: String,
+    password: Password,
+}
+#[derive(FromForm, Debug)]
+struct Password(String);
+#[derive(FromForm, Debug)]
+struct CalcVars {
     num1: f32,
-    num2: f32
+    num2: f32,
 }
 
+
 #[post("/note", data = "<new_note>")]
-fn create_new_note(new_note: Form<NewNote>) -> Template {
+async fn create_new_note(new_note: Form<NewNote>) -> Template {
     let title = String::from("Note created!");
     println!("{:?}", new_note.content);
     Template::render("note_created",
@@ -50,26 +56,41 @@ fn create_new_note(new_note: Form<NewNote>) -> Template {
 }
 
 #[get("/")]
-fn index() -> Redirect {
+async fn index() -> Redirect {
     Redirect::to(uri!(home))
 }
 #[get("/home")]
-fn home() -> Template {
+async fn home() -> Template {
     let title = String::from("Home");
     Template::render("test_template_base",
                      context! { title: title } )
 }
 #[get("/note/new")]
-fn write_note() -> Template {
+async fn write_note() -> Template {
     let title = String::from("New Note");
     Template::render("create_note",
                      context! { title: title } )
 }
+#[get("/user/login")]
+async fn login_page() -> Template {
+    let title = String::from("Login");
+    Template::render("login_form",
+                     context! { title: title } )
+}
+#[post("/login", data = "<user_auth>")]
+async fn login(jar: &CookieJar<'_>, user_auth: Form<User>) -> Status {
+    jar.add(("user_name", user_auth.username.to_owned()));
+    Status::Ok
+}
 #[get("/calculator")]
-fn calculator() -> Template {
+async fn calculator() -> Template {
     let title = String::from("Calculator");
     Template::render("calculator",
                      context! { title: title } )
+}
+#[post("/add", data = "<calc_vars>")]
+async fn relay_add(calc_vars: Form<CalcVars>) -> Redirect {
+    Redirect::to(uri!(add(calc_vars.num1, calc_vars.num2)))
 }
 
 #[get("/add/<num1>/<num2>")]
@@ -115,24 +136,23 @@ fn example() -> Template {
 //    Template::render("first_extend_template",
 //                     context! { users: users, title: title } )
 //}
-#[get("/<_..>", rank = 12)]
-fn not_found() -> NotFound {
-    NotFound("could not find path")
-}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![home])
+        .mount("/", routes![login])
+        .mount("/", routes![login_page])
         .mount("/", routes![write_note])
         .mount("/", routes![create_new_note])
         .mount("/", routes![teapot])
         .mount("/", routes![example])
         .mount("/", routes![calculator])
+        .mount("/", routes![relay_add])
         .mount("/", routes![add])
         .mount("/", routes![multi])
         .mount("/assets/", routes![serve_css])
-        .mount("/", routes![not_found])
         .attach(Template::fairing())
+        .register("/", catchers![not_found])
 }
